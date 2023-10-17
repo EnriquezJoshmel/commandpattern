ece919ad0df382a3488d83b834cdbfc8dc1f26cb
EnriquezJoshmel <142742663+EnriquezJoshmel@users.noreply.github.com>
2023-10-18 00:03:21 +0800

    Update Tv.java

--- commandPattern/Tv.java
+++ commandPattern/Tv.java
@@ -1,7 +1,6 @@
 package commandPattern;
 
 public class Tv {
-
     public String switchOn(){
         return "Tv is switched-on!";
 
@@ -9,5 +8,40 @@ public class Tv {
 
     public String switchOff(){
         return "Tv is switched-off!";
+    }
+
+    public String increaseVol(){
+        return "Sound Sytem volume increase!";
+
+    }
+    public String decreaseVol(){
+        return "Sound Sytem volume decrease!";
+
+    }
+    public String tempUp(){
+        return "Aircon temperature increase!";
+
+    }
+    public String tempDown(){
+        return "Aircon temperature increase";
+
+    }
+
+    public String brightRise(){
+        return "LED Lights increase!";
+
+    }
+
+    public String brightDeduct(){
+        return "LED Lights decrease!";
+
+    }
+    public String playSong(){
+        return "Your playlist now playing!";
+    }
+
+    public String exit(){
+        return "Goodbye!";
+
     }
 }
